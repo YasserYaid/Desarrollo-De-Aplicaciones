12177d8fb8f0d4428cd0e398403fccddd537b461
YasserYaid <108897183+YasserYaid@users.noreply.github.com>
2023-06-16 21:01:21 -0600

    Actividad7

---
+++ Actividad07/app/src/main/java/com/example/monitor/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.monitor;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.monitor.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
+        binding.eqRecycler.setLayoutManager(new LinearLayoutManager(this));
+
+        EqAdapter adapter = new EqAdapter();
+        binding.eqRecycler.setAdapter(adapter);
+        adapter.submitList(eqList);///ESTA PARTE NO LA ENTENDI DEL TUTORIAL
+    }
+}
